e0387ee6f7766aaada4ed1f7a5102c696556d5fe
gduquesne02 <Ag74Evs7M>
2023-01-03 09:26:14 +0100

    Commit du 07/12/2022 à 13h20 par Antoine Lamesch

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,15 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_map/flutter_map.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:latlong2/latlong.dart';
+import 'package:littlewords/WordCount.dart';
 import 'package:littlewords/dio.provider.dart';
+import 'package:littlewords/routes/Error.route.dart';
+import 'package:littlewords/routes/Loading.route.dart';
+import 'package:littlewords/routes/Username.route.dart';
+import 'package:littlewords/shared_pref.provider.dart';
 import 'package:littlewords/version.dart';
 
 
@@ -10,28 +17,33 @@ void main() {
   runApp(ProviderScope(child: const MyApp()));
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends ConsumerWidget {
   const MyApp({super.key});
 
   // This widget is the root of your application.
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: ref.watch(usernameProvider).when(data: _data, error: _error, loading: _loading)
     );
+  }
+  Widget _data(String? username) {
+    if (null == username) {
+      return UsernameRoute();
+    }
+
+    print('username $username');
+    return MyHomePage(title: 'littlewords');
+  }
+  Widget _error(error, stack){
+    return ErrorRoute();
+  }
+  Widget _loading() {
+    return LoadingRoute();
   }
 }
 
@@ -63,24 +75,33 @@ class _MyHomePageState extends State<MyHomePage> {
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
       body: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        crossAxisAlignment: CrossAxisAlignment.end,
+        mainAxisAlignment: MainAxisAlignment.center,
+
         children : [
 
           Version(),
 
-          Version()
+          WordCount(),
 
-
-        ],
-     ),
-    );
+          FlutterMap(
+            options:
+            MapOptions(center: LatLng(_position.latitude, _position.longitude), zoom: 10),
+            nonRotatedChildren: [
+              AttributionWidget.defaultWidget(
+                source: 'OpenStreetMap contributors',
+                onSourceTapped: null,
+              ),
+            ],
+            children: [
+              TileLayer(
+                urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+                userAgentPackageName: 'com.example.app',
+                subdomains: ['a', 'b', 'c'],
+              ),
+            ],
+          ),
+      );
 
   }
 }
